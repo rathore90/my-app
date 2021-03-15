5945a818a2b1d89840977ddb97656725f61a957e
rathore90 <rathore90@hotmail.com>
2021-03-15 13:41:05 -0700

    callback function

--- src/posts/PostDetail.js
+++ src/posts/PostDetail.js
@@ -2,11 +2,22 @@ import React, { Component } from 'react'
 import '../App.css'
 
 class PostDetails extends Component {
+    constructor(props){
+        super(props)
+        this.titleWasClicked = this.titleWasClicked.bind(this)
+    }
+    titleWasClicked(event){
+        event.preventDefault()
+        const {dataCallback} = this.props
+        if(dataCallback !== undefined){
+            dataCallback("Hello World") 
+        }
+    }
     render(){
         const {post} = this.props    
     return (
             <div className="card-holder">
-                <h1>{post.email}</h1>
+                <h1 onClick={this.titleWasClicked}>{post.email}</h1>
                 <p>{post.created}</p>
             </div>
         )
--- src/posts/PostList.js
+++ src/posts/PostList.js
@@ -4,12 +4,22 @@ import '../App.css'
 import PostDetail from './PostDetail'
 
 class PostList extends Component {
+    constructor(props){
+        super(props)
+        this.handleDataCallback = this.handleDataCallback.bind(this)
+    }
+    handleDataCallback(txtMsg){
+        alert(txtMsg)
+        console.log(this)
+    }
     render(){    
     return (
         <div className="App">
             <h1>Imagine Everything</h1>
             {PostData.map((postDetails, index)=>{
-                return <PostDetail post={postDetails} key={'psot-list-key ${index}'}/>
+                return <PostDetail 
+                        post={postDetails} key={'psot-list-key ${index}'} 
+                        dataCallback={this.handleDataCallback}/>
             })}
         </div>
         )
