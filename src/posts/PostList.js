4178f7a3f66c46e4fafd44162efa42680eba1a9c
rathore90 <rathore90@hotmail.com>
2021-03-15 13:00:24 -0700

    pull all the data

--- src/posts/PostList.js
+++ src/posts/PostList.js
@@ -1,13 +1,17 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 import PostData from '../data/audit.json'
+import '../App.css'
 
 class PostList extends Component {
     render(){    
     return (
         <div className="App">
-            <h1>Hello There</h1>
+            <h1>Imagine Everything</h1>
             {PostData.map((postDetails, index)=>{
-                return <h1>{postDetails.email}</h1>
+                return <div className="card-holder">
+                            <h1>{postDetails.email}</h1>
+                            <p>{postDetails.created}</p>
+                        </div>
             })}
         </div>
     )
